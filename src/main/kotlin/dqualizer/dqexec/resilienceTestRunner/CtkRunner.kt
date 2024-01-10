78955933bad320a896f9f53064358943f2f3a8c1
Henning Möllers <henning.moellers@wps.de>
2024-01-10 10:27:01 +0100

    Make CtkRunner executable in Docker Container.

--- src/main/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunner.kt
+++ src/main/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunner.kt
@@ -64,15 +64,17 @@ class CtkRunner(
 
         for (chaosExperiment in config.ctkChaosExperiments) {
             val jsonPayload = objectMapper.writeValueAsString(chaosExperiment)
-            val experimentFilePath = Path("C:/Users/HenningMöllers/IdeaProjects/dqexec/src/main/resources/ctk/generatedExperiments/${chaosExperiment.title.replace(" ", "")}_experiment.json")
+            //val experimentFilePath = Path("C:/Users/HenningMöllers/IdeaProjects/dqexec/src/main/resources/ctk/generatedExperiments/${chaosExperiment.title.replace(" ", "")}_experiment.json")
+            val experimentFilePath = Path("/app/generatedExperiments/${chaosExperiment.title.replace(" ", "")}_experiment.json")
             saveJsonToFile(jsonPayload, experimentFilePath)
-            logger.info("### EXPERIMENT $testCounter WAS CREATED IN $experimentFilePath ###")
+            logger.info("### CHAOS EXPERIMENT $testCounter WAS CREATED IN $experimentFilePath ###")
             var runCounter = 1
 
             //repeat one loadtest if as many times as specified in the configuration
             while (runCounter <= chaosExperiment.repetitions){
-                val exitValue = runTestOnLocalWindowsChaosToolkit(experimentFilePath, testCounter, runCounter)
-                logger.info("### Chaos Experiment $testCounter-$runCounter FINISHED WITH VALUE $exitValue ###")
+                //val exitValue = runExperimentOnLocalWindowsChaosToolkit(experimentFilePath, testCounter, runCounter)
+                val exitValue = runExperiment(experimentFilePath, testCounter, runCounter)
+                logger.info(" CHAOS EXPERIMENT $testCounter-$runCounter FINISHED WITH VALUE $exitValue ###")
                 runCounter++
             }
             testCounter++
@@ -83,6 +85,8 @@ class CtkRunner(
     fun saveJsonToFile(jsonPayload: String, filePath: Path) {
         try {
             val file = filePath.toFile()
+            // Create the parent directories if they don't exist
+            file.parentFile.mkdirs()
             file.writeText(jsonPayload)
 
         } catch (e: Exception) {
@@ -93,8 +97,7 @@ class CtkRunner(
 
     // TODO make private again later
     @Throws(IOException::class, InterruptedException::class)
-    public fun runTestOnLocalWindowsChaosToolkit(experimentPath: Path, testCounter: Int, runCounter: Int): Int {
-        // TODO use trigger in docker container (makes mounting of filepath necessary...) OR trigger in local venv?!
+    public fun runExperimentOnLocalWindowsChaosToolkit(experimentPath: Path, testCounter: Int, runCounter: Int): Int {
 
         val activateVenvCommand = """C:\Users\HenningMöllers\IdeaProjects\Dqualizer_Resilienz_Manuelle_Uebersetzung\venv\Scripts\activate.ps1"""
 
@@ -124,6 +127,36 @@ class CtkRunner(
         while (reader.readLine().also { line = it } != null) {
             println(line)
         }*/
+
+        val exitCode = process.waitFor()
+        println("Exit Code: $exitCode")
+        return exitCode
+    }
+
+    // TODO make private again later
+    @Throws(IOException::class, InterruptedException::class)
+    public fun runExperiment(experimentPath: Path, testCounter: Int, runCounter: Int): Int {
+
+        val executeExperimentCommand = """chaos run $experimentPath"""
+
+        logger.info(
+                """
+            ### RUN COMMAND: $executeExperimentCommand ###
+            """.trimIndent()
+        )
+
+        val process = ProcessBuilder(executeExperimentCommand).inheritIO().start()
+
+        val loggingPath = paths.getLogFilePath(testCounter, runCounter)
+        processLogger.log(process, loggingPath)
+
+        // TODO Is already logged by processLogger?!
+        // Capture and print the output and error streams
+        /* val reader = BufferedReader(InputStreamReader(process.inputStream))
+         var line: String?
+         while (reader.readLine().also { line = it } != null) {
+             println(line)
+         }*/
 
         val exitCode = process.waitFor()
         println("Exit Code: $exitCode")
--- src/test/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunnerTest.kt
+++ src/test/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunnerTest.kt
@@ -4,7 +4,6 @@ import dqualizer.dqexec.config.ResourcePaths
 import dqualizer.dqexec.util.ProcessLogger
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.mock
 import kotlin.io.path.Path
 
 class CtkRunnerTest {
@@ -18,7 +17,7 @@ class CtkRunnerTest {
         val experimentPath = Path("resources\\ctk\\generatedExperiments\\TestDescription_experiment.json")
 
         //act
-        val result = ctkRunner.runTestOnLocalWindowsChaosToolkit(experimentPath, 1, 1 )
+        val result = ctkRunner.runExperimentOnLocalWindowsChaosToolkit(experimentPath, 1, 1 )
 
         //assert
         Assertions.assertEquals(0, result)
