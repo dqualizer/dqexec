f3d7f14f3fd1b2bafa169a36c528f8386b582be2
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-05-17 14:06:37 +0200

    Remove unused code.

--- src/main/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunner.kt
+++ src/main/kotlin/dqualizer/dqexec/resilienceTestRunner/CtkRunner.kt
@@ -2,10 +2,8 @@ package dqualizer.dqexec.resilienceTestRunner
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import dqualizer.dqexec.config.ResourcePaths
 import dqualizer.dqexec.exception.RunnerFailedException
 import dqualizer.dqexec.util.EnvironmentChecker
-import dqualizer.dqexec.util.ProcessLogger
 import io.github.dqualizer.dqlang.types.adapter.ctk.CtkConfiguration
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -13,18 +11,13 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.io.IOException
-import java.nio.file.Path
 import java.util.logging.Logger
-import kotlin.io.path.Path
-import kotlin.io.path.exists
 
 /**
  *
  */
 @Service
-class CtkRunner(
-        private val processLogger: ProcessLogger,
-        private val paths: ResourcePaths) {
+class CtkRunner() {
     private val logger = Logger.getLogger(this.javaClass.name)
 
 
@@ -50,14 +43,6 @@ class CtkRunner(
     private fun run(config: CtkConfiguration) {
         logger.info("Trying to run configuration: " + ObjectMapper().writeValueAsString(config))
 
-        /*var baseURL = config.baseURL
-        val localhostMatcher = "localhost|127\\.0\\.0\\.1".toRegex()
-        //If config-runner runs inside docker, localhost can´t be used so its replaced by the alternative host
-        if (alternativeTargetHost.isNotBlank() && localhostMatcher.containsMatchIn(config.baseURL)) {
-            baseURL = config.baseURL.replace(localhostMatcher, alternativeTargetHost)
-            logger.info("Alternative host was provided (\$dqualizer.dqexec.docker.localhost_replacement): Replacing 'localhost' or '127.0.0.1' in ${config.baseURL} with $alternativeTargetHost. Result: $baseURL")
-        }*/
-
         var testCounter = 1
         val objectMapper = ObjectMapper()
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT)
@@ -73,60 +58,8 @@ class CtkRunner(
         logger.info("### RESILIENCE TESTING COMPLETE ###")
     }
 
-    fun saveJsonToFile(jsonPayload: String, filePath: Path) {
-        try {
-            val file = filePath.toFile()
-            // Create the parent directories if they don't exist
-            file.parentFile.mkdirs()
-            file.writeText(jsonPayload)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-
-    // TODO make private again later
     @Throws(IOException::class, InterruptedException::class)
-    public fun runExperimentOnLocalWindowsChaosToolkit(experimentPath: Path, testCounter: Int, runCounter: Int): Int {
-
-        val activateVenvCommand = """C:\Users\HenningMöllers\IdeaProjects\Dqualizer_Resilienz_Manuelle_Uebersetzung\venv\Scripts\activate.ps1"""
-
-        val executeExperimentCommand = """chaos run $experimentPath"""
-        val activateAndExecuteCommand = "$activateVenvCommand ; $executeExperimentCommand"
-
-      /*  println(activateVenvCommand)
-        println(executeExperimentCommand)
-        println(activateAndExecuteCommand)*/
-
-        logger.info(
-                """
-            ### RUN COMMAND: $activateAndExecuteCommand ###
-            """.trimIndent()
-        )
-
-        val process = ProcessBuilder("""C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe""",
-                "-Command",
-                activateAndExecuteCommand).inheritIO().start()
-
-        val loggingPath = paths.getLogFilePath(testCounter, runCounter)
-        processLogger.log(process, loggingPath)
-
-        // Capture and print the output and error streams
-       /* val reader = BufferedReader(InputStreamReader(process.inputStream))
-        var line: String?
-        while (reader.readLine().also { line = it } != null) {
-            println(line)
-        }*/
-
-        val exitCode = process.waitFor()
-        println("Exit Code: $exitCode")
-        return exitCode
-    }
-
-    // TODO make private again later
-    @Throws(IOException::class, InterruptedException::class)
-    fun requestExperimentExecutionOnHost(experimentJson: String, testCounter: Int): Int {
+    private fun requestExperimentExecutionOnHost(experimentJson: String, testCounter: Int): Int {
 
         val restTemplate = RestTemplate()
         var url = ""
