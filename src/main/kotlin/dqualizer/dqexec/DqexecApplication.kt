f19223c907c33dad842b255034d0d6b9bc6843f2
EddeCCC <eduard.schander@gmx.de>
2024-07-18 12:08:07 +0200

    add mongo annotation

--- src/main/kotlin/dqualizer/dqexec/DqexecApplication.kt
+++ src/main/kotlin/dqualizer/dqexec/DqexecApplication.kt
@@ -2,7 +2,9 @@ package dqualizer.dqexec
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
+@EnableMongoRepositories
 @SpringBootApplication
 class DqexecApplication
 
